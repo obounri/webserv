a9161447eb9084a85c14af75272d567da52ca7c5
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-11 23:03:33 +0100

    parsing conf file

--- srcs/parsers/parse_config.cpp
+++ srcs/parsers/parse_config.cpp
@@ -1,22 +1,47 @@
-#include <vector>
-
 #include "parse_config.hpp"
+
+void	comment_check(std::string& line)
+{
+	size_t	found;
+	found = line.find('#');
+	if (found != std::string::npos)
+		line = line.erase(found);
+	return ;
+}
+
+void server_block(t_parse vars)
+{
+	size_t found;
+	if (!vars.server_stat && !vars.line.empty() && (found = vars.line.find("server")) == std::string::npos)
+		throw std::runtime_error(unexpected_token(vars.line));
+	// else
+	// {
+	// 	size_t pos = vars.line.find_first_of("{");
+	// 	// if (pos == std::string::npos)
+	// 	// 	throw std::runtime_error(unexpected_token(vars.line.substr()));
+	// }
+	return ;
+}
 
 void parse_config(char *config_path)
 {
-	std::ifstream   conf_file;
-	std::string     line;
-	conf_file.open(config_path);
-	if (conf_file.is_open())
+	t_parse vars = {};
+	vars.conf_file.open(config_path);
+	if (vars.conf_file.is_open())
 	{
-		while (std::getline(conf_file, line))
+		while (std::getline(vars.conf_file, vars.line))
         {
-			if (!line.empty())
-				std::cout << line << std::endl;
+			if (!vars.line.empty())
+			{
+				comment_check(vars.line);
+				vars.line.erase(std::remove_if(vars.line.begin(), vars.line.end(), isspace), vars.line.end());
+				// server_block(vars);
+				std::cout << vars.line << std::endl;
+			}
         }
 	}
 	else
-		std::cerr << "File not found\n";
-	conf_file.close();
+		throw std::runtime_error("Error : File not found");
+	vars.conf_file.close();
 	return ;
 }
--- srcs/parsers/parse_config.hpp
+++ srcs/parsers/parse_config.hpp
@@ -5,28 +5,62 @@
 #include <fstream>
 #include <cstring>
 #include <arpa/inet.h>
+#include <exception>
+#include <algorithm>
 #include <vector>
 
+// class m_errors : public std::exception
+// {
+//     private :
+//                 std::string _error_msg;
+//     public :
+//                 m_errors(std::string error_msg) : _error_msg(error_msg) {}
+//                 virtual ~m_errors() {}
+//                 virtual const char* what() const throw()
+//                 {
+//                     return _error_msg.c_str();
+//                 }
+// };
+typedef struct		s_parse
+{
+	std::ifstream	conf_file;
+	std::string		line;
+	int				server_stat;
+	int				location_stat;
+
+}					t_parse;
 
 typedef struct s_server
 {
-    int port;
-    std::string root;
-    // std::string ;
+	int port;
+	std::string root;
+	std::string host;
 }           v_server;
 
 struct config
 {
-    /* data */
-    // int domain = AF_INET;
-    // int type = SOCK_STREAM;
-    // int interface = INADDR_ANY;
-    // int backlog;
-    int n_v_servers;
-    // v_server    *vservers;
-    std::vector<v_server> vservers;
+	/* data */
+	// int domain = AF_INET;
+	// int type = SOCK_STREAM;
+	// int interface = INADDR_ANY;
+	// int backlog;
+	// v_server    *vservers;
+
+	int n_v_servers;
+	std::vector<v_server> vservers;
 };
 
 void    parse_config(char *config_path);
+
+// Syntax Error
+std::string unexpected_token(std::string &name);
+std::string missing_close_brace(std::string &name);
+std::string missing_open_brace(std::string &name);
+std::string missing_semicolon(std::string &name);
+std::string too_many_arguments(std::string &name);
+std::string no_server(std::string &name);
+std::string missing_arguments(std::string &name);
+std::string duplicate_key(std::string &name);
+std::string duplicate_location(std::string &name);
 
 #endif
