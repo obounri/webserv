6c33e6bd651b5b8edc4768101f75c87d8d03569d
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-14 00:15:56 +0100

    Add : new function for conf parsing : left_brace listen(in progress)

--- srcs/parsers/parse_config.cpp
+++ srcs/parsers/parse_config.cpp
@@ -9,23 +9,79 @@ void	comment_check(std::string& line)
 	return ;
 }
 
-void server_block(t_parse vars)
+void	left_brace_check(t_parse &vars , size_t *pos)
 {
-	size_t found;
-	if (!vars.server_stat && !vars.line.empty() && (found = vars.line.find("server")) == std::string::npos)
-		throw std::runtime_error(unexpected_token(vars.line));
-	// else
+	size_t cmp = vars.tokens[*pos].compare("{");
+	if (vars.server_state.server == 1 && cmp)
+		throw std::runtime_error(missing_open_brace(vars.tokens[*pos]));
+	else if (vars.server_state.server == 2 && !cmp)
+		throw std::runtime_error(duplicate_key(vars.tokens[*pos]));
+	else if (!cmp)
+	{
+		vars.server_state.server = 2;
+		(*pos)++;
+	}
+}
+
+void	listen(t_parse &vars, size_t *pos)
+{
+	size_t cmp = vars.tokens[*pos].compare("listen");
+	if (vars.server_state.server != 2 && !cmp)
+		throw std::runtime_error(unexpected_token(vars.tokens[*pos]));
+	if (!cmp && vars.tokens[*pos + 1].empty())
+		throw std::runtime_error(missing_arguments(vars.tokens[*pos]));
+	else if (!cmp)
+	{
+		std::cout << "listen and their arg found after server \n";
+		(*pos)++;
+	}
+}
+
+void	server_block(t_parse *vars, config *configs)
+{
+	size_t pos = 0;
+	size_t cmp = vars->tokens[pos].compare("server");
+	if (vars->server_state.server == 0 && cmp)
+		throw std::runtime_error(unexpected_token(vars->tokens[pos]));
+	else if (!vars->server_state.server) // if server_state 0
+	{
+		vars->server_state.server = 1;
+		pos++;
+		configs->n_v_servers++;
+	}
+	while (!vars->tokens[pos].empty())
+	{
+		left_brace_check(*vars, &pos);
+		listen(*vars, &pos);
+		pos++;
+	}
+	// if (vars->server_state)
 	// {
-	// 	size_t pos = vars.line.find_first_of("{");
-	// 	// if (pos == std::string::npos)
-	// 	// 	throw std::runtime_error(unexpected_token(vars.line.substr()));
+	// 	size_t pos = vars->line.find_first_of("{");
+	// 	if (pos == std::string::npos)
+	// 		throw std::runtime_error(unexpected_token(vars->line));
 	// }
 	return ;
+}
+
+void	split_line(t_parse *vars, std::string const delimiters)
+{
+	size_t begin = 0 , pos;
+
+	while ((pos = vars->line.find_first_of(delimiters, begin)) != std::string::npos)
+	{
+		if (pos > begin)
+			vars->tokens.push_back(vars->line.substr(begin, pos - begin));
+		begin = pos + 1;
+	}
+	if (begin < vars->line.length())
+		vars->tokens.push_back(vars->line.substr(begin, std::string::npos));
 }
 
 void parse_config(char *config_path)
 {
 	t_parse vars = {};
+	config	configs = {};
 	vars.conf_file.open(config_path);
 	if (vars.conf_file.is_open())
 	{
@@ -34,9 +90,12 @@ void parse_config(char *config_path)
 			if (!vars.line.empty())
 			{
 				comment_check(vars.line);
-				vars.line.erase(std::remove_if(vars.line.begin(), vars.line.end(), isspace), vars.line.end());
-				// server_block(vars);
-				std::cout << vars.line << std::endl;
+				split_line(&vars, " \t");
+				if (!vars.tokens.empty())
+					server_block(&vars , &configs);
+				for(size_t i= 0; i < vars.tokens.size(); i++)
+					std::cout << "token : |" << vars.tokens[i] << "|\n";
+				vars.tokens.clear();
 			}
         }
 	}
