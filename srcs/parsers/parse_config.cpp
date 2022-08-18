782b1307b492ff02bba00b24ae1292c1c87946be
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-18 23:08:34 +0100

    parsing conf

--- srcs/parsers/parse_config.cpp
+++ srcs/parsers/parse_config.cpp
@@ -9,75 +9,6 @@ void	comment_check(std::string& line)
 	return ;
 }
 
-void	left_brace_check(t_parse &vars , size_t *pos)
-{
-	size_t cmp = vars.tokens[*pos].compare("{");
-	if (vars.server_state.server == 1 && cmp)
-		throw std::runtime_error(missing_open_brace(vars.tokens[*pos]));
-	else if (vars.server_state.server == 2 && !cmp)
-		throw std::runtime_error(duplicate_key(vars.tokens[*pos]));
-	else if (!cmp)
-	{
-		vars.server_state.server = 2;
-		(*pos)++;
-	}
-}
-
-void	listen(t_parse &vars, size_t *pos)
-{
-	size_t cmp = vars.tokens[*pos].compare("listen");
-	if (vars.server_state.server != 2 && !cmp)
-		throw std::runtime_error(unexpected_token(vars.tokens[*pos]));
-	if (!cmp && vars.tokens[*pos + 1].empty())
-		throw std::runtime_error(missing_arguments(vars.tokens[*pos]));
-	else if (!cmp)
-	{
-		std::cout << "listen and their arg found after server \n";
-		(*pos)++;
-	}
-}
-
-void	server_block(t_parse *vars, config *configs)
-{
-	size_t pos = 0;
-	size_t cmp = vars->tokens[pos].compare("server");
-	if (vars->server_state.server == 0 && cmp)
-		throw std::runtime_error(unexpected_token(vars->tokens[pos]));
-	else if (!vars->server_state.server) // if server_state 0
-	{
-		vars->server_state.server = 1;
-		pos++;
-		configs->n_v_servers++;
-	}
-	while (!vars->tokens[pos].empty())
-	{
-		left_brace_check(*vars, &pos);
-		listen(*vars, &pos);
-		pos++;
-	}
-	// if (vars->server_state)
-	// {
-	// 	size_t pos = vars->line.find_first_of("{");
-	// 	if (pos == std::string::npos)
-	// 		throw std::runtime_error(unexpected_token(vars->line));
-	// }
-	return ;
-}
-
-void	split_line(t_parse *vars, std::string const delimiters)
-{
-	size_t begin = 0 , pos;
-
-	while ((pos = vars->line.find_first_of(delimiters, begin)) != std::string::npos)
-	{
-		if (pos > begin)
-			vars->tokens.push_back(vars->line.substr(begin, pos - begin));
-		begin = pos + 1;
-	}
-	if (begin < vars->line.length())
-		vars->tokens.push_back(vars->line.substr(begin, std::string::npos));
-}
-
 void parse_config(char *config_path)
 {
 	t_parse vars = {};
@@ -90,12 +21,14 @@ void parse_config(char *config_path)
 			if (!vars.line.empty())
 			{
 				comment_check(vars.line);
-				split_line(&vars, " \t");
+				vars.tokens = split_line(vars.line, " \t");
 				if (!vars.tokens.empty())
-					server_block(&vars , &configs);
+					server_block(vars , configs);
 				for(size_t i= 0; i < vars.tokens.size(); i++)
 					std::cout << "token : |" << vars.tokens[i] << "|\n";
 				vars.tokens.clear();
+			std::cout << "port : " << vars.tmp_server.port << "\n";
+			std::cout << "host : " << vars.tmp_server.host << "\n";
 			}
         }
 	}
--- srcs/parsers/parse_config.hpp
+++ srcs/parsers/parse_config.hpp
@@ -8,6 +8,7 @@
 #include <exception>
 #include <algorithm>
 #include <vector>
+#include <map>
 
 // class m_errors : public std::exception
 // {
@@ -27,29 +28,34 @@ typedef	struct		s_state
 	int				left_brace;
 	int				right_brace;
 	int				listen;
+	int				host;
+	int				client_max_body_size;
 	int				server_name;
 	int				root;
+	int				location;
 	int				index;
+}					serv_state;
 
-}					t_state;
+
+typedef struct						s_server
+{
+	int								port;
+	std::string						host;
+	std::string						server_name;
+	int								client_max_body_size;
+	std::string						index;
+	std::string						root;
+	std::map<int, std::string>		error_pages;
+}									v_server;
 
 typedef struct		s_parse
 {
 	std::ifstream				conf_file;
 	std::string					line;
 	std::vector<std::string>	tokens;
-	t_state						server_state;
-	int							location_stat;
-
+	serv_state					server_state;
+	v_server					tmp_server;
 }					t_parse;
-
-typedef struct		s_server
-{
-	int				port;
-	std::string		root;
-	std::string		host;
-}					v_server;
-
 struct config
 {
 	/* data */
@@ -63,19 +69,36 @@ struct config
 	std::vector<v_server> vservers;
 };
 
+
 // SETION - Config File Parsing 
 
-void    parse_config(char *config_path);
+void    					parse_config(char *config_path);
+void						comment_check(std::string& line);
+void						server_block(t_parse &vars, config &configs);
+void						left_brace(t_parse &vars , size_t &pos);
+void						listen(t_parse &vars, size_t &pos);
+void						set_listen_port(t_parse &vars, size_t &pos, int & found);
+void						host(t_parse &vars, size_t &pos);
+void						check_set_ip(t_parse &vars, size_t &pos);
+
+
+// SUBSECTION - Parsing utils
+
+bool						IsNumber(std::string &str);
+bool						Is_IP_Adress(std::string &str);
+std::vector<std::string> 	split_line(std::string  const &line, std::string const &delimiters);
+
 
 // SECTION - Syntax Error
-std::string unexpected_token(std::string &name);
-std::string missing_close_brace(std::string &name);
-std::string missing_open_brace(std::string &name);
-std::string missing_semicolon(std::string &name);
-std::string too_many_arguments(std::string &name);
-std::string no_server(std::string &name);
-std::string missing_arguments(std::string &name);
-std::string duplicate_key(std::string &name);
-std::string duplicate_location(std::string &name);
+std::string					Syntax_error(std::string const &name);
+std::string 				unexpected_token(std::string const &name);
+std::string 				missing_close_brace(std::string const &name);
+std::string 				missing_open_brace(std::string const &name);
+std::string 				missing_semicolon(std::string const &name);
+std::string 				too_many_arguments(std::string const &name);
+std::string 				no_server(std::string const &name);
+std::string 				missing_arguments(std::string const &name);
+std::string 				duplicate_key(std::string const &name);
+std::string 				duplicate_location(std::string const &name);
 
 #endif
