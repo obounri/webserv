a791de072a7006a259cda97a8fc877cded3d8c03
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-25 00:43:39 +0100

    Edit : some syntax error functions

--- srcs/parsers/Syntax_errors.cpp
+++ srcs/parsers/Syntax_errors.cpp
@@ -7,15 +7,13 @@ std::string unexpected_token(std::string const &name) { return std::string("Synt
 
 std::string missing_close_brace(std::string const &name) { return std::string("Syntax Error : Missing close brace '" + name + "'"); }
 
-std::string missing_open_brace(std::string const &name) { return std::string("Syntax Error : Missing open brace '" + name + "'"); }
-
-std::string missing_semicolon(std::string const &name) { return std::string("Syntax Error : Missing ';' '" + name + "'"); }
+std::string missing_open_brace(std::string const &name) { return std::string("Syntax Error : Missing open brace after '" + name + "'"); }
 
 std::string too_many_arguments(std::string const &name) { return std::string("Syntax Error : too many arguments '" + name + "'"); }
 
 std::string no_server(std::string const &name) { return std::string("Syntax Error : no server to fill with '" + name + "'"); }
 
-std::string missing_arguments(std::string const &name) { return std::string("Syntax Error : Missing arguments to '" + name + "'"); }
+std::string Invalid_arguments(std::string const &name) { return std::string("Syntax Error : Invalid number of arguments '" + name + "'"); }
 
 std::string duplicate_key(std::string  const &name) { return std::string("Syntax Error : duplicate key '" + name + "'"); }
 
