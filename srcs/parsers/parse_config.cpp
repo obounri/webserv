364426b4ee11f61d243730d7ad14f76a4bda4e69
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-25 00:45:31 +0100

    Add : print fuctions for testing and debugins

--- srcs/parsers/parse_config.cpp
+++ srcs/parsers/parse_config.cpp
@@ -9,6 +9,35 @@ void	comment_check(std::string& line)
 	return ;
 }
 
+void	print_conf(t_parse &vars)
+{
+	std::cout << "\n=============== configs ==================\n";
+	if (vars.tmp_server.port)
+		std::cout << "listen : " << vars.tmp_server.port << "\n";
+	if (!vars.tmp_server.host.empty())
+		std::cout << "host : " << vars.tmp_server.host << "\n";
+	if (!vars.tmp_server.server_name.empty())
+		std::cout << "server_name : " << vars.tmp_server.server_name << "\n";
+	if (!vars.tmp_server.client_max_body_size)
+		std::cout << "client body : " << vars.tmp_server.client_max_body_size << "\n";
+	if (!vars.tmp_server.root.empty())
+		std::cout << "root : " << vars.tmp_server.root << "\n";
+	if (!vars.tmp_server.index.empty())
+		std::cout << "index : " << vars.tmp_server.index << "\n";
+	if (!vars.tmp_server.error_pages.empty())
+	{
+		for (std::map<int , std::string>::iterator it = vars.tmp_server.error_pages.begin(); it != vars.tmp_server.error_pages.end(); it++)
+			std::cout << "error " << (*it).first << "\t" << (*it).second << std::endl;
+	}
+	if (!vars.tmp_location.autoindex)
+		std::cout << "autoindex off\n";
+	else
+		std::cout << "autoindex on\n";
+	if (!vars.tmp_location.index.empty())
+	std::cout << "location index : " << vars.tmp_location.index << "\n";
+	std::cout << "\n=========================================\n";
+}
+
 void parse_config(char *config_path)
 {
 	t_parse vars = {};
@@ -23,12 +52,14 @@ void parse_config(char *config_path)
 				comment_check(vars.line);
 				vars.tokens = split_line(vars.line, " \t");
 				if (!vars.tokens.empty())
-					server_block(vars , configs);
-				for(size_t i= 0; i < vars.tokens.size(); i++)
-					std::cout << "token : |" << vars.tokens[i] << "|\n";
-				vars.tokens.clear();
-			std::cout << "port : " << vars.tmp_server.port << "\n";
-			std::cout << "host : " << vars.tmp_server.host << "\n";
+				{
+					if (vars.server_state.location)
+						location_block(vars);
+					else
+						server_block(vars, configs);
+					vars.tokens.clear();
+					print_conf(vars);
+				}
 			}
         }
 	}
