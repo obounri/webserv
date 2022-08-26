dba95bad8a9906338fd176f32ed08277ebb00fb0
obounri <othmane.bounri@gmail.com>
2022-08-26 16:00:21 +0100

    requests and responses come and go in chunks of max size MAX_IO_SIZE

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -39,13 +39,11 @@ void    Server::accept_new_connection(unsigned long int fd)
     socklen_t   addr_size;
     addr_size = sizeof their_addr;
 
-    std::cout << "accept_new_connection" << std::endl;
     newfd = accept(fd, (sockaddr *)&their_addr, &addr_size);
     clients.push_back(client(newfd, inet_ntoa(their_addr.sin_addr), ntohs(their_addr.sin_port)));
     EV_SET(&evSet, newfd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, &clients[clients.size()-1]);
     kevent(keq, &evSet, 1, NULL, 0, NULL);
-    std::cout << "got connection request from fd = " << newfd << ".." << std::endl;
-    std::cout << inet_ntoa(their_addr.sin_addr) << std::endl;
+    std::cout << "Got connection request from fd = " << newfd << ".." << inet_ntoa(their_addr.sin_addr) << std::endl;
     std::cout << ntohs(their_addr.sin_port) << std::endl << std::endl;
 }
 
@@ -59,16 +57,17 @@ void    Server::destroy_connection(int fd, int event)
     close(fd);
 }
 
-void    Server::handle_request(client *c)
+void    Server::recv_request(client *c)
 {
     size_t pos;
+    size_t rec;
     struct kevent evSet;
     std::string tmp;
 
-    bzero(c->recBuffer, MAX_RECV_SIZE);
-    if ((c->rec = recv(c->fd, c->recBuffer, MAX_RECV_SIZE - 1, 0)) > 0) {
+    bzero(c->recBuffer, MAX_IO_SIZE);
+    if ((rec = recv(c->fd, c->recBuffer, MAX_IO_SIZE - 1, 0)) > 0) {
         c->req.append(c->recBuffer);
-        std::cout << " << " << c->rec;
+        std::cout << " << " << rec;
     }
     else {
         std::cout << "reading failed.." << std::endl << std::endl;
@@ -91,6 +90,8 @@ void    Server::handle_request(client *c)
         std::cout << "\ncompleted request from fd " << c->fd << std::endl << "HEADER:\n" << c->header << std::endl << "BODY\n" << c->req << std::endl;
         c->req.clear();
         c->header.clear();
+        c->sendBuffer = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 3280\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed velit velit, convallis vel hendrerit ut, facilisis scelerisque lorem. Praesent quis lectus eu lorem tincidunt iaculis nec ac diam. Quisque scelerisque felis felis, nec varius ipsum placerat nec. Donec faucibus tellus a sollicitudin pretium. In ornare aliquet pretium. Donec vestibulum ut enim id mattis. Morbi ullamcorper auctor leo, a ultricies nunc maximus sit amet. Donec facilisis orci tortor, cursus tincidunt sem sodales tristique. Nam lacinia, mi ut aliquam varius, risus massa scelerisque tortor, id tristique ipsum dui placerat enim. Mauris vehicula commodo ex vel varius. Sed molestie ultricies sodales. Phasellus quam sem, sagittis non ornare et, posuere sit amet ligula. Vivamus dignissim mi sit amet sapien suscipit, sed sollicitudin metus faucibus. Nullam vitae ultricies ante. Quisque faucibus mi sit amet ligula suscipit tincidunt. Nunc sed augue pharetra, efficitur velit blandit, interdum est.\nSed suscipit viverra erat, sollicitudin fringilla orci tempus et. Vestibulum pellentesque nulla sed scelerisque venenatis. Cras augue velit, dapibus vel luctus sit amet, interdum ut erat. Praesent ac eros at tellus egestas luctus vitae vel nisi. Aliquam erat volutpat. Fusce felis neque, dignissim ac commodo non, euismod in est. Mauris maximus ac purus vel convallis. Sed efficitur vitae mauris ut tristique. Nam mi augue, elementum quis mi et, mattis lacinia leo. Etiam tempor hendrerit enim vel venenatis. Nullam molestie metus et sem ultricies elementum.\nPellentesque eget tortor vel neque rutrum sodales. Morbi vitae cursus nibh. Curabitur quis magna eu lorem venenatis tempus non non leo. Duis eget suscipit neque. Etiam finibus nunc massa, vel venenatis elit imperdiet fringilla. Pellentesque non quam vitae arcu pretium ullamcorper eu at metus. Donec euismod interdum sem, a volutpat nulla molestie quis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed rhoncus turpis sit amet tellus aliquam, vel fringilla odio eleifend. Ut non ultrices mi. Maecenas diam augue, semper cursus ultricies at, hendrerit ac mi.\nSed elementum faucibus arcu, sed consequat metus luctus sed. Ut non metus risus. Nam eget blandit diam, at ultricies elit. Sed eget tortor vel erat varius semper eget sit amet neque. Vivamus dignissim maximus nulla, nec semper sapien luctus at. Curabitur ac lorem id odio fringilla sollicitudin. Nullam lobortis varius neque, eu feugiat purus faucibus quis. Donec scelerisque elit ut lacus ullamcorper, quis accumsan sapien elementum. Praesent interdum et ligula lacinia accumsan. Sed ligula enim, cursus ut erat ac, tristique ullamcorper erat.\nQuisque dictum sodales tortor in pulvinar. Sed malesuada sollicitudin felis. Vestibulum hendrerit mi metus, vel dictum diam fermentum vel. Morbi vestibulum sodales sapien, nec sagittis odio porttitor sit amet. Mauris at urna ultricies, tempus magna ac, consequat justo. Etiam suscipit leo nisi, eget bibendum dolor elementum a. Vestibulum id orci massa. Ut ligula leo, malesuada in imperdiet vehicula, facilisis laoreet erat. Aenean hendrerit quis arcu eget luctus. Nullam ornare urna consequat ligula ultricies pharetra. Nulla aliquet dictum tortor ac sodales. Quisque nec ipsum leo. Ut varius enim quis accumsan fermentum.\r\n\r\n";
+        c->sent = 0;
         EV_SET(&evSet, c->fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
         kevent(keq, &evSet, 1, NULL, 0, NULL);
         EV_SET(&evSet, c->fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, c);
@@ -100,22 +101,28 @@ void    Server::handle_request(client *c)
 
 void    Server::send_request(client *c)
 {
-    int sent;
+    int sent = 0, to_send = c->sendBuffer.length() - c->sent;
     struct kevent evSet;
 
-    std::cout << ">> fd " << c->fd << " is ready for writing" << std::endl;
-    if ((sent = send(c->fd, DUMMY_HTTP_RESPONSE, sizeof DUMMY_HTTP_RESPONSE, 0)) != -1) {
-        std::cout << "message sent = " << sent << std::endl << std::endl;;
+    if (to_send > MAX_IO_SIZE)
+        to_send = MAX_IO_SIZE;
+    if ((sent = send(c->fd, c->sendBuffer.c_str() + c->sent, to_send, 0)) != -1) {
+        std::cout << " >> " << sent;
+        c->sent += sent;
     }
     else {
         std::cout << "sending failed.." << std::endl << std::endl;
         destroy_connection(c->fd, EVFILT_WRITE);
         return ;
     }
-    EV_SET(&evSet, c->fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
-    kevent(keq, &evSet, 1, NULL, 0, NULL);
-    EV_SET(&evSet, c->fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, c);
-    kevent(keq, &evSet, 1, NULL, 0, NULL);
+    std::cout << std::endl << c->sent << std::endl;
+    if (c->sent == c->sendBuffer.length()) {
+        std::cout << "\nSent full request" << std::endl;
+        EV_SET(&evSet, c->fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
+        kevent(keq, &evSet, 1, NULL, 0, NULL);
+        EV_SET(&evSet, c->fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, c);
+        kevent(keq, &evSet, 1, NULL, 0, NULL);
+    }
 }
 
 void Server::run() {
@@ -124,17 +131,15 @@ void Server::run() {
 
     std::cout << "server up and running.." << std::endl << std::endl;
     for(;;) {
-        // std::cout << "waiting for events.." << std::endl;
         if ((num_events = kevent(keq, NULL, 0, evList, 32, NULL)) == -1)
             throw std::runtime_error("");
-        // std::cout << "catched " << num_events << " event(s).." << std::endl;
         for (int i = 0; i < num_events; i++) {
             if (is_listener(evList[i].ident))
                 accept_new_connection(evList[i].ident);
             else if (evList[i].flags & EV_EOF)
                 destroy_connection(evList[i].ident, EVFILT_READ);
             else if (evList[i].filter == EVFILT_READ)
-                handle_request((client *)evList[i].udata);
+                recv_request((client *)evList[i].udata);
             else if (evList[i].filter == EVFILT_WRITE)
                 send_request((client *)evList[i].udata);
         }
--- srcs/server/server.hpp
+++ srcs/server/server.hpp
@@ -12,8 +12,7 @@
 #endif
     
 
-#define MAX_RECV_SIZE 1024
-#define DUMMY_HTTP_RESPONSE "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello world!\r\n\r\n"
+#define MAX_IO_SIZE 1024
 
 typedef struct s_client
 {
@@ -21,12 +20,15 @@ typedef struct s_client
     int         port;
     std::string ip;
 
-    char        recBuffer[MAX_RECV_SIZE];
+    char        recBuffer[MAX_IO_SIZE];
     std::string header;
     std::string req;
-    int         rec;
+    // int         rec;
     size_t      body_len;
-    // std::string send;
+
+    // Respone res;
+    size_t         sent;
+    std::string sendBuffer;
 
     s_client(int _fd, std::string _ip, int _port):fd(_fd), port(_port), ip(_ip) {} ;
 } client;
@@ -34,14 +36,13 @@ typedef struct s_client
 class Server
 {
     private:
-        /* data */
         std::vector<Socket *>   listeners;
         std::vector<client>     clients;
         int                     keq;
 
         void    accept_new_connection(unsigned long int fd);
         void    destroy_connection(int fd, int event);
-        void    handle_request(client *c);
+        void    recv_request(client *c);
         void    send_request(client *c);
 
         int     is_listener(unsigned long int fd);
@@ -51,8 +52,5 @@ class Server
 
         void            run();
 };
-
-// int kevent(int kq, const struct kevent *changelist, int nchanges, struct kevent *eventlist, int nevents, const struct timespec *timeout);
-// EV_SET(&kev, ident, filter, flags, fflags, data, udata);
 
 #endif
