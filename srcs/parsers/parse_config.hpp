f59f473c51ffab14bf8634d54603f2ceeb7fca72
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-08-28 23:52:18 +0100

    add v_server class (class for parsing server block)

--- srcs/parsers/parse_config.hpp
+++ srcs/parsers/parse_config.hpp
@@ -7,24 +7,142 @@
 #include <arpa/inet.h>
 #include <exception>
 #include <algorithm>
+#include <list>
 #include <vector>
 #include <map>
 #include "location.hpp"
-#include "../server/server.hpp"
+// #include "../server/server.hpp"
+class v_server
+{
+	private:
+		// Default
+		std::string host;
+		int	port;
+		std::string name;
+		std::string root;
+		std::string default_page;
+		int limit_body;
+		std::string limit_body_s;
+		std::list<std::string> methods;
+		std::map<std::string ,location> locations;
+		std::map<std::string, std::string> cgi;
+		std::map<std::string ,std::string> extra_headers;
+		std::string file_open;
+        // headers header_var;
 
-// class location;
-// class m_errors : public std::exception
-// {
-//     private :
-//                 std::string _error_msg;
-//     public :
-//                 m_errors(std::string error_msg) : _error_msg(error_msg) {}
-//                 virtual ~m_errors() {}
-//                 virtual const char* what() const throw()
-//                 {
-//                     return _error_msg.c_str();
-//                 }
-// };
+		// Server
+		int server_fd;
+		struct sockaddr_in address;
+
+		// Errors
+		int par_error;
+		int page_error_val;
+		// t_error_messages _err;
+		std::map<std::string, std::string> error_pg; // error pages
+
+		// Request
+		int qu_len;
+		std::string file_type;
+		std::string pwd_dir;
+		std::string req_method;
+		std::string qu_string;
+		// t_file_log f_log;
+
+
+		// Clients
+		std::string client_ip;
+		// std::vector<client*> clients;
+
+		
+	public:
+		v_server(/* args */);
+		~v_server();
+
+        // Set Methods
+		void set_host(std::string str);
+		void set_port(std::string str);
+		void set_name(std::string str);
+		void set_limit(std::string str);
+		void set_methods(std::string str);
+		void set_cgi(std::string name, std::string path);
+		void set_location(std::vector<std::string>::iterator &it, std::vector<std::string>::iterator end);
+		void set_error(std::vector<std::string> res);
+		void cgi_parse(std::vector<std::string>::iterator it, std::vector<std::string>::iterator end);
+		void set_root(std::string str);
+		void set_qu_string(std::string str);
+		void set_pg_val(int val);
+		void set_method(std::string val);
+		void set_pwd(std::string p);
+		void set_qu_len(std::string val);
+		void set_access(std::string val);
+		void set_client_ip(std::string val);
+		
+        // Get Methods
+		std::string get_host();
+		std::string get_port();
+		std::list<std::string> get_methods();
+		std::string get_string_methods();
+		std::string get_name();
+		std::string get_cgi();
+		std::string get_file_type();
+		std::string get_root();
+		std::string get_error_page(int val);
+		std::string get_method();
+		std::string get_req_file(std::string val, std::string met);
+		std::string get_file(std::map<std::string, location>::iterator loc, std::string val);
+		std::map<std::string ,location> get_location();
+		// file_log get_f_log();
+		// headers get_header_var(void);
+		std::string get_body_s();
+		std::string ft_read_file(std::string val);
+		int get_fd_server();
+		struct sockaddr_in get_server_address();
+		int get_limit();
+        int value_port();
+		int get_pg_error();
+
+		// Extra
+		bool check();
+        void print_server();
+		char **setup_env();
+		// cgi_env ft_add_other_headers();
+		std::string get_name_headers(std::map<std::string, std::string>::iterator it);
+
+		
+        //  Launch
+        void run_server();
+		std::string run_error(int val);
+        void set_header(char *buff);
+        void reset_header();
+		bool ft_allowed();
+		std::string ft_cgi_exist();
+		void update_location();
+		int ft_access();
+
+		// requests methods
+		std::string run_file();
+		std::string run_method();
+		std::string put_method();
+		std::string delete_method();
+		std::string options_method();
+		std::string run_cgi();
+		std::string post_method();
+
+		void cgi_status_code(std::string response);
+		void update_port(std::string val);
+};
+
+typedef struct s_config
+{
+    /* data */
+    static const int domain = AF_INET;
+    static const int type = SOCK_STREAM;
+    static const int interface = INADDR_ANY;
+    int backlog;
+    int n_v_servers;
+
+    std::vector<v_server> vservers;
+} config;
 
 
 // SECTION - Server State Structure
@@ -69,17 +187,17 @@ typedef	struct							l_state
 
 // SECTION - Server Block Structure
 
-typedef struct							s_server
-{
-	int									port;
-	std::string							host;
-	std::string							server_name;
-	int									client_max_body_size;
-	std::string							index;
-	std::string							root;
-	std::map<int, std::string>			error_pages;
-	std::map<std::string, location>		location;
-}										v_server;
+// typedef struct							s_server
+// {
+// 	int									port;
+// 	std::string							host;
+// 	std::string							server_name;
+// 	int									client_max_body_size;
+// 	std::string							index;
+// 	std::string							root;
+// 	std::map<int, std::string>			error_pages;
+// 	std::map<std::string, location>		location;
+// }										v_server;
 
 // SECTION - Config File Parsing Structure
 
@@ -91,8 +209,8 @@ typedef struct							s_parse
 	serv_state							server_state;
 	loc_state							location_state;
 	location							tmp_location;
-	v_server							tmp_server;
-	Server								tmp_serv;
+	// v_server							tmp_server;
+	v_server							tmp_serv;
 
 }										t_parse;
 
